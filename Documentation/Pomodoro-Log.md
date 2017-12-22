9881a929279ae0b959c67ebe23260e28e624ed7e
Stefan Hoffmann <Stefan.Hoffmann@mahr-edv.de>
2017-12-22 14:03:34 +0100

    new player "class" to control player ui independently, experiments with the styles and the ui to understand what is needed to make the ui work and what is already there

--- Documentation/Pomodoro-Log.md
+++ Documentation/Pomodoro-Log.md
@@ -3,3 +3,4 @@
 1. Repository setup
 1. Documentation of the projects requirements
 1. Added jquery, added new code file, finished R1 and started R2
+1. new player "class" to control player ui independently, experiments with the styles and the ui to understand what is needed to make the ui work and what is already there
--- Source/js/tic-tac-toe.js
+++ Source/js/tic-tac-toe.js
@@ -4,21 +4,43 @@
     github/stho32
 */
 
-(() => {
-    const $startScreen = $("#start");
-    const $startScreenButton = $("#screen-start-button");
-    const $board = $("#board");
+const $startScreen = $("#start");
+const $startScreenButton = $("#screen-start-button");
+const $board = $("#board");
 
-    /* (R2) at startup the board is hidden and the start screen is displayed */
-    function StartupScreenInteraction() {
-        $board.hide()
-        $startScreen.show();
+/* (R2) at startup the board is hidden and the start screen is displayed */
+function StartupScreenInteraction() {
+    $board.hide()
+    $startScreen.show();
 
-        $startScreenButton.on("click", () => {
-            $startScreen.hide();
-            $board.show();
-        });
-    }
+    $startScreenButton.on("click", () => {
+        $startScreen.hide();
+        $board.show();
+    });
+}
 
-    StartupScreenInteraction();
-})();
+StartupScreenInteraction();
+
+function Player(id, sign) {
+    let $headerSign = $("#player" + id);
+    let isActive = false;
+
+    let publicApi = {
+        Id : id,
+        Sign: sign
+    };
+
+    publicApi.setActive = (active) => {
+        isActive = active;
+
+        $headerSign.toggleClass("active");
+    };
+
+    return publicApi;
+}
+
+let playerO = Player(1, "O");
+let playerX = Player(2, "X");
+
+playerO.setActive(true);
+
