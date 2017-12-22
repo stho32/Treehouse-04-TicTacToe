b94ec41684a582b3346166b45166cc701e939707
Stefan Hoffmann <sthoffmann@onlinehome.de>
2017-12-22 07:21:12 +0100

    R2 is fullfilled

--- Documentation/Pomodoro-Log.md
+++ Documentation/Pomodoro-Log.md
@@ -2,3 +2,4 @@
 
 1. Repository setup
 1. Documentation of the projects requirements
+1. Added jquery, added new code file, finished R1 and started R2
--- Source/js/tic-tac-toe.js
+++ Source/js/tic-tac-toe.js
@@ -3,3 +3,22 @@
 
     github/stho32
 */
+
+(() => {
+    const $startScreen = $("#start");
+    const $startScreenButton = $("#screen-start-button");
+    const $board = $("#board");
+
+    /* (R2) at startup the board is hidden and the start screen is displayed */
+    function StartupScreenInteraction() {
+        $board.hide()
+        $startScreen.show();
+
+        $startScreenButton.on("click", () => {
+            $startScreen.hide();
+            $board.show();
+        });
+    }
+
+    StartupScreenInteraction();
+})();
