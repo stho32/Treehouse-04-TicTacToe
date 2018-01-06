92e3b619a2d7d0836aab185338e68a1af5ea1a62
Stefan Hoffmann <sthoffmann@onlinehome.de>
2018-01-06 07:41:43 +0100

    The unit tests now tell me the ai should work.

--- Source-AI/main.js
+++ Source-AI/main.js
@@ -8,15 +8,20 @@
  * This function permutates through the possible next states of the game. 
  * It adds some meta information that we need to choose a nice next move.
  * 
- * @param {string} boardAsString The gameboard represeted as one continuous string of the characters "XO ".
  * @param {string} aiPlayerSign Sign of the player that the AI is playing. The sign is either X or O.
  */
-function AI_CalculateNextSteps(boardAsString, aiPlayerSign) {
+function AI_CalculateNextSteps(aiPlayerSign) {
     let publicApi = {};
 
-    let start = boardAsString;
+    /**
+     * possibleMovesFromHere is the "result" that is generated via the permutations
+     * we call AI. The info consists of a move info (place sign x at position y) and 
+     * a point number determined by the needed count of moves, the count of the possible wins vs.
+     * the count of the possible losses taking this direction.  
+     */
     let possibleMovesFromHere = [];
     let activePlayer = aiPlayerSign;
+    let iAmPlayer = aiPlayerSign;
 
     /* We want to return a new string that contains our new
        choosen character at position <index>.
@@ -79,33 +84,97 @@ function AI_CalculateNextSteps(boardAsString, aiPlayerSign) {
 
     publicApi.DetectWinningSituation = detectWinningSituation;
      
-
-    function permutation(start, index, player) {
-        //console.log("Its players " + player + " turn: " + start);
+    /**
+     * Permutate the possible moves and outcomes and collect them.
+     * 
+     * @param {string} start The boards state as string "X  OX ...", to be mutated.
+     *                       Every recursive call changes this value.
+     * @param {number} index Set a sign at that position
+     * @param {string} player Either X or O, "you are that player at this moment", the sign to be set
+     * @param {number} firstNextMoveIndex In this method we explore possible options into depth. But when all calculation
+     *                                    is done, we do not want to know, that there are final states X and Y but what
+     *                                    is the exact next move to some day reach the desired final states.
+     *                                    So while start is changed at every recursive call, we only generate "firstNextMove" 
+     *                                    once upon the time and then remember it. It is the index for the possibleNextMoves
+     *                                    array. 
+     */
+    function permutation(start, index, player, firstNextMoveIndex) {
         let myState = start;
 
         if (index !== undefined) {
-            let newPlayer = player;
+            myState = replaceCharX(start, index, player);
+
+            if ( firstNextMoveIndex === undefined ) {
+                /* Right now we calculated the "firstNextMove", so we better remember it.
+                */
+                var myFirstNextMove = {
+                    sign : player,
+                    position: index,
+                    points: 0
+                };
+
+                possibleMovesFromHere.push(myFirstNextMove);
+                firstNextMoveIndex = possibleMovesFromHere.length-1;
+            }
+        }
+
+        if ( firstNextMoveIndex !== undefined ) {
+            // Do we have a winner?
+            let winner = detectWinningSituation(myState);
+            if ( winner !== "" ) {
+                if (winner === iAmPlayer) possibleMovesFromHere[firstNextMoveIndex].points += 10;
+                if (winner !== iAmPlayer) possibleMovesFromHere[firstNextMoveIndex].points -= 10;
+                
+                // end recursion
+                return;
+            }
+
+            // Do we have a tie?
+            if ( noFurtherEmptyPlaces(myState) ) {
+                // end recursion
+                return;
+            }
+        }
+
+        let newPlayer = player;
+        /* If we are in the first recursion layer there is no index found yet. 
+           Then we do not want to change the player, as this turn is only for finding
+           the next free spaces. 
+           If we have an index, then the next player is the other player... 
+        */
+        if ( index !== undefined ) {
             if (player === "X") {
                 newPlayer = "O";
             } else {
                 newPlayer = "X";
             }
-                
-            myState = replaceCharX(start, index, player);
         }
 
-        let winner = detectWinningSituation(myState);
-
-        if ( noFurtherEmptyPlaces(myState) ) {
-            console.log("final state : " + myState);
-        } else {
-            for (let i = 0; i < myState.length; i ++) {
-                if ( myState[i] === " " ) {
-                    permutation(myState, i, newPlayer);
-                }
+        // We have neither a winning state nor a tie. Thus we permutate along
+        // to find the next moves and calculate their points.
+        for (let i = 0; i < myState.length; i ++) {
+            if ( myState[i] === " " ) {
+                permutation(myState, i, newPlayer, firstNextMoveIndex);
             }
         }
+    };
+
+    publicApi.CalculatePossibleMoves = function(board) {
+        possibleMovesFromHere = [];
+        permutation(board, undefined, aiPlayerSign, undefined);
+        return possibleMovesFromHere;
+    }
+
+    publicApi.CalculateNextMove = function(board) {
+        publicApi.CalculatePossibleMoves(board);
+
+        /* Sort possible moves by descending points... 
+           "Choose the most winning path." */
+        possibleMovesFromHere.sort(function(a,b) {
+            return b.points - a.points;
+        });
+
+        return possibleMovesFromHere[0];
     };
 
     return publicApi;
--- Source-AI/tests.js
+++ Source-AI/tests.js
@@ -50,3 +50,60 @@ QUnit.test("DetectWinningSituation recognizes winning diagonals", function(asser
     assert.ok(ai.DetectWinningSituation("X   X   X") === "X", "X wins, diagonal 1");
     assert.ok(ai.DetectWinningSituation("  X X X  ") === "X", "X wins, diagonal 2");
 });
+
+
+QUnit.test("CalculatePossibleMoves : On board XXO OOX XO  only 1 move is possible and its useless (points===0)", function(assert) {
+    let nextMoves = AI_CalculateNextSteps("X").CalculatePossibleMoves(
+        "XXO" +
+        "OOX" + 
+        "XO "
+    );
+
+    assert.ok(nextMoves.length === 1, "There is only one move possible.");
+    assert.ok(nextMoves[0].sign === "X", "Its the AI's move." );
+    assert.ok(nextMoves[0].points === 0, "Its a useless move (tie).");
+    assert.ok(nextMoves[0].position === 8, "8 is the index of the last column in the last row.")
+});
+
+QUnit.test("CalculatePossibleMoves : On board XOO OXO XO  only 1 move is possible and its a win (points===10)", function(assert) {
+    let nextMoves = AI_CalculateNextSteps("X").CalculatePossibleMoves(
+        "XOO" +
+        "OXO" + 
+        "XO "
+    );
+
+    assert.ok(nextMoves.length === 1, "There is only one move possible.");
+    assert.ok(nextMoves[0].sign === "X", "Its the AI's move." );
+    assert.ok(nextMoves[0].points === 10, "Its a winning move.");
+    assert.ok(nextMoves[0].position === 8, "8 is the index of the last column in the last row.")
+});
+
+QUnit.test("CalculatePossibleMoves : On board XOO OXO X   2 moves are possible, one is a win for the other guy", function(assert) {
+    let nextMoves = AI_CalculateNextSteps("X").CalculatePossibleMoves(
+        "XOO" +
+        "OXO" + 
+        "X  "
+    );
+
+    assert.ok(nextMoves.length === 2, "Two moves are possible.");
+
+    assert.ok(nextMoves[0].sign === "X", "Its the AI's move." );
+    assert.ok(nextMoves[0].position === 7, "Placing the X on position 7...")
+    assert.ok(nextMoves[0].points === -10, "...looses you the game");
+
+    assert.ok(nextMoves[1].sign === "X", "Its the AI's move." );
+    assert.ok(nextMoves[1].position === 8, "Placing the X on position 8...")
+    assert.ok(nextMoves[1].points === 10, "...wins you the game");
+});
+
+QUnit.test("CalculateNextMove : On board XOO OXO X   get me the winning move!", function(assert) {
+    let move = AI_CalculateNextSteps("X").CalculateNextMove(
+        "XOO" +
+        "OXO" + 
+        "X  "
+    );
+
+    assert.ok(move.sign === "X", "Its the AI's move." );
+    assert.ok(move.position === 8, "Placing the X on position 8...")
+    assert.ok(move.points === 10, "...wins you the game");
+});
