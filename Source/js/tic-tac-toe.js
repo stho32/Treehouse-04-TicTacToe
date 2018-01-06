e074a835a1774367a153e93a739c21a74ac3c05e
Stefan Hoffmann <sthoffmann@onlinehome.de>
2018-01-06 07:56:21 +0100

    Included AI from Source-AI folder.

--- Source/js/tic-tac-toe.js
+++ Source/js/tic-tac-toe.js
@@ -223,12 +223,205 @@ function HumanPlayerInteraction(player, gameboard) {
     });
 }
 
+/** 
+ * AI SOURCE CODE --- DEVELOPED AND TESTED in Source-AI folder -------------------------------------
+ */
+/**
+ * This function permutates through the possible next states of the game. 
+ * It adds some meta information that we need to choose a nice next move.
+ * 
+ * @param {string} aiPlayerSign Sign of the player that the AI is playing. The sign is either X or O.
+ */
+function AI_CalculateNextSteps(aiPlayerSign) {
+    let publicApi = {};
+
+    /**
+     * possibleMovesFromHere is the "result" that is generated via the permutations
+     * we call AI. The info consists of a move info (place sign x at position y) and 
+     * a point number determined by the needed count of moves, the count of the possible wins vs.
+     * the count of the possible losses taking this direction.  
+     */
+    let possibleMovesFromHere = [];
+    let activePlayer = aiPlayerSign;
+    let iAmPlayer = aiPlayerSign;
+
+    /* We want to return a new string that contains our new
+       choosen character at position <index>.
+    */
+    function replaceCharX(text, index, newChar) {
+        return text.substring(0, index) + newChar + 
+               text.substring(index +1);
+    }
+    
+    publicApi.ReplaceCharX = replaceCharX;
+    
+    /* Thats how we detect a final state. If there are no 
+       empty places left on a board then its done. */
+    function noFurtherEmptyPlaces(text) {
+        // if this command does not change the string length then
+        // we have no spaces left.
+        return (text.length === text.replace(" ", "").length)
+    }
+
+    publicApi.NoFurtherEmptyPlaces = noFurtherEmptyPlaces;
+
+    /* Even before we hit the possible "all places taken", we might encounter
+       a winning situation. Although there are still whitespaces
+       they are final states.
+
+       @returns {string} X or O or empty string, depending on if a winner is found
+    */
+    function detectWinningSituation(board) {
+        function containsOnly(text, sign) {
+            if ( text.length === 0 )
+                return false;
+
+            return text.replace(new RegExp(sign,"g"), "") === "";
+        }
+
+        // Rows, Columns and Diagonals...
+        let sequences = [];
+        // Rows
+        sequences.push( board[0] + board[1] + board[2] );
+        sequences.push( board[3] + board[4] + board[5] );
+        sequences.push( board[6] + board[7] + board[8] );
+        // Columns
+        sequences.push( board[0] + board[3] + board[6] );
+        sequences.push( board[1] + board[4] + board[7] );
+        sequences.push( board[2] + board[5] + board[8] );
+        // Diagonals
+        sequences.push( board[0] + board[4] + board[8] );
+        sequences.push( board[2] + board[4] + board[6] );
+
+        let result = ""; 
+
+        ["X", "O"].forEach( sign => {
+            sequences.forEach( sequence => {
+                if ( containsOnly(sequence, sign) ) return result = sign;
+            });
+        });
+
+        return result;
+    }
+
+    publicApi.DetectWinningSituation = detectWinningSituation;
+     
+    /**
+     * Permutate the possible moves and outcomes and collect them.
+     * 
+     * @param {string} start The boards state as string "X  OX ...", to be mutated.
+     *                       Every recursive call changes this value.
+     * @param {number} index Set a sign at that position
+     * @param {string} player Either X or O, "you are that player at this moment", the sign to be set
+     * @param {number} firstNextMoveIndex In this method we explore possible options into depth. But when all calculation
+     *                                    is done, we do not want to know, that there are final states X and Y but what
+     *                                    is the exact next move to some day reach the desired final states.
+     *                                    So while start is changed at every recursive call, we only generate "firstNextMove" 
+     *                                    once upon the time and then remember it. It is the index for the possibleNextMoves
+     *                                    array. 
+     */
+    function permutation(start, index, player, firstNextMoveIndex) {
+        let myState = start;
+
+        if (index !== undefined) {
+            myState = replaceCharX(start, index, player);
+
+            if ( firstNextMoveIndex === undefined ) {
+                /* Right now we calculated the "firstNextMove", so we better remember it.
+                */
+                var myFirstNextMove = {
+                    sign : player,
+                    position: index,
+                    points: 0
+                };
+
+                possibleMovesFromHere.push(myFirstNextMove);
+                firstNextMoveIndex = possibleMovesFromHere.length-1;
+            }
+        }
+
+        if ( firstNextMoveIndex !== undefined ) {
+            // Do we have a winner?
+            let winner = detectWinningSituation(myState);
+            if ( winner !== "" ) {
+                if (winner === iAmPlayer) possibleMovesFromHere[firstNextMoveIndex].points += 10;
+                if (winner !== iAmPlayer) possibleMovesFromHere[firstNextMoveIndex].points -= 10;
+                
+                // end recursion
+                return;
+            }
+
+            // Do we have a tie?
+            if ( noFurtherEmptyPlaces(myState) ) {
+                // end recursion
+                return;
+            }
+        }
+
+        let newPlayer = player;
+        /* If we are in the first recursion layer there is no index found yet. 
+           Then we do not want to change the player, as this turn is only for finding
+           the next free spaces. 
+           If we have an index, then the next player is the other player... 
+        */
+        if ( index !== undefined ) {
+            if (player === "X") {
+                newPlayer = "O";
+            } else {
+                newPlayer = "X";
+            }
+        }
+
+        // We have neither a winning state nor a tie. Thus we permutate along
+        // to find the next moves and calculate their points.
+        for (let i = 0; i < myState.length; i ++) {
+            if ( myState[i] === " " ) {
+                permutation(myState, i, newPlayer, firstNextMoveIndex);
+            }
+        }
+    };
+
+    publicApi.CalculatePossibleMoves = function(board) {
+        possibleMovesFromHere = [];
+        permutation(board, undefined, aiPlayerSign, undefined);
+        return possibleMovesFromHere;
+    }
+
+    publicApi.CalculateNextMove = function(board) {
+        publicApi.CalculatePossibleMoves(board);
+
+        /* Sort possible moves by descending points... 
+           "Choose the most winning path." */
+        possibleMovesFromHere.sort(function(a,b) {
+            return b.points - a.points;
+        });
+
+        return possibleMovesFromHere[0];
+    };
+
+    return publicApi;
+}
+
+/*  ---------------------------------------------------------------------------------------------- */
+
 /* This is how a computer player makes a move. */
 function ComputerPlayerInteraction(player, gameboard) {
     // 1. activate some animation that tells the user to wait
     // 2. calculate and think
-    // 3. place sign
+    // 2.1 translate the board for the AI
+    let board = gameboard.GetBoardAsString();
+    let ai = AI_CalculateNextSteps(player.Sign);
+    let move = ai.CalculateNextMove(board);
+    // 3. place sign 
+    // We need to translate the index of the AI to the column/row info we need on the 
+    // board. 
+    let row = Math.ceil(move.index / 3);
+    let column = move.index - (3*row);
+    row +=1;
+    column +=1;
+    gameboard.PlaceSignAtPosition(row, column);
     // 4. give control back to the gameboard ("next player")
+    gameboard.ContinueGameplay();
 }
 
 /* "make a move" */
@@ -365,6 +558,28 @@ function GameboardScene() {
     publicApi.AreAllBoxesFilled = () => {
         return publicApi.GetEmptyBoxes().length === 0;
     };
+
+    publicApi.GetBoardAsString = () => {
+        let result = "";
+        let boxes = $board.find(".box");
+
+        for(let i = 0; i < boxes.length; i++ ) {
+            // Box with O as O
+            if ( $(boxes[i]).hasClass(publicApi.PlayerO.PlayerSignCssClass) ) {
+                result += publicApi.PlayerO.Sign;
+                continue;
+            }
+            // Box with X as X
+            if ( $(boxes[i]).hasClass(publicApi.PlayerX.PlayerSignCssClass) ) {
+                result += publicApi.PlayerX.Sign;
+                continue;
+            }
+            // Empty box
+            result += " ";
+        }
+
+        return result;
+    }
 
 
     /* Uses a list of coordinates to validate, if there is a 
